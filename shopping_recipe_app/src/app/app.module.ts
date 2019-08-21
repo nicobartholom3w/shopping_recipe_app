f9a31778710320d6f7a3eed019f70c8f0765e850
Nicole Bartholomew <nabartholomew@gmail.com>
2019-08-21 10:44:21 -0500

    (feat) Added header, recipes, shopping-list, recipe-list, recipe-item, shopping-list-edit, & ingredient components.
    Also added some html and scss to header component.

--- shopping_recipe_app/src/app/app.module.ts
+++ shopping_recipe_app/src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './header/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './header/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { IngredientComponent } from './header/shopping-list/ingredient/ingredient.component';
+import { RecipesComponent } from './header/recipes/recipes.component';
+import { RecipeListComponent } from './header/recipes/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    IngredientComponent,
+    RecipesComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule,
