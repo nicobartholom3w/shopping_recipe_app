82f0eb022713ec9d7b16503425be4216cf932edb
Nicole Bartholomew <nabartholomew@gmail.com>
2019-09-04 19:05:26 -0500

    (feat) Added functionality to Recipes and Shopping list hrefs in heading.

--- shopping_recipe_app/src/app/header/header.component.ts
+++ shopping_recipe_app/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  @Output() menuChange = new EventEmitter<string>()
 
+  changeMenu(feature) {
+    this.menuChange.emit(feature)
+  }
   constructor() { }
 
   ngOnInit() {
