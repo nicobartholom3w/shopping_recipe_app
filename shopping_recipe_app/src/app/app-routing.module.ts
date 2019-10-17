b5d6d35205ef1180c4806bad8a779714494de9e5
Nicole Bartholomew <nabartholomew@gmail.com>
2019-10-17 12:15:44 -0500

    (feat) Added Back to recipes button from shopping list.

--- shopping_recipe_app/src/app/app-routing.module.ts
+++ shopping_recipe_app/src/app/app-routing.module.ts
@@ -4,16 +4,18 @@ import { HomeComponent } from './home/home.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-item/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 
 
 const appRoutes: Routes = [
     // { path: '', component: AppComponent },
-    { path: '', component: HomeComponent },
+    { path: '', component: HomeComponent, pathMatch: 'full' },
     { path: 'recipes', component: RecipesComponent, children: [] },
     { path: 'shopping-list', component: ShoppingListComponent },
     { path: '*', component: HomeComponent },
     { path: 'recipes/recipe-detail/:id', component: RecipeDetailComponent },
+    { path: 'recipes/recipe-detail/:id/edit', component: RecipeEditComponent }
   ]
 
   @NgModule({
